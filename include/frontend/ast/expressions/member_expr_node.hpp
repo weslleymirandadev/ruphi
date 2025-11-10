d6ca09331e147078f9cd2ae5ca8040aa7230d836
weslleymirandadev <contato@weslleymiranda.dev>
2025-11-10 11:21:04 -0300

    feat(member): added member expr

---
+++ include/frontend/ast/expressions/member_expr_node.hpp
@@ -0,0 +1,23 @@
+#pragma once
+#include "../types.hpp"
+#include <memory>
+
+class MemberExprNode : public Expr {
+public:
+    std::unique_ptr<Expr> object;
+    std::unique_ptr<Expr> property;
+
+    MemberExprNode(std::unique_ptr<Expr> object, std::unique_ptr<Expr> property)
+        : Expr(NodeType::MemberExpression), object(std::move(object)), property(std::move(property)) {}
+
+    ~MemberExprNode() override = default;
+
+    Node* clone() const override {
+        auto cloned_object = object ? std::unique_ptr<Expr>(static_cast<Expr*>(object->clone())) : nullptr;
+        auto cloned_property = property ? std::unique_ptr<Expr>(static_cast<Expr*>(property->clone())) : nullptr;
+        return new MemberExprNode(std::move(cloned_object), std::move(cloned_property));
+    }
+
+    void codegen(rph::IRGenerationContext& ctx) override;
+};
+
