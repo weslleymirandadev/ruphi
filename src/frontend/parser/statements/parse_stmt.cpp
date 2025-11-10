afc2255a6bc0f6c846945a3a66bf8e5ffcb24341
weslleymirandadev <contato@weslleymiranda.dev>
2025-11-10 11:28:08 -0300

    feat(statements): added statements parser

---
+++ src/frontend/parser/statements/parse_stmt.cpp
@@ -0,0 +1,28 @@
+#include "frontend/parser/statements/parse_stmt.hpp"
+#include "frontend/parser/statements/parse_label_stmt.hpp"
+#include "frontend/parser/expressions/parse_expr.hpp"
+#include "frontend/parser/statements/parse_if_stmt.hpp"
+#include "frontend/parser/statements/parse_return_stmt.hpp"
+#include "frontend/parser/statements/parse_for_stmt.hpp"
+#include "frontend/parser/statements/parse_loop_stmt.hpp"
+#include "frontend/parser/statements/parse_while_stmt.hpp"
+#include "frontend/parser/statements/parse_match_stmt.hpp"
+
+std::unique_ptr<Node> parse_stmt(Parser* parser) {
+    switch (parser->current_token().type) {
+        case TokenType::LABEL: return parse_label_stmt(parser);
+        case TokenType::IF: return parse_if_stmt(parser);
+        case TokenType::RETURN: return parse_return_stmt(parser);
+        case TokenType::FOR: return parse_for_stmt(parser);
+        case TokenType::LOOP: return parse_loop_stmt(parser);
+        case TokenType::WHILE: return parse_while_stmt(parser);
+        case TokenType::MATCH: return parse_match_stmt(parser);
+        default: {
+            auto node = parse_expr(parser);
+            if (parser->current_token().type == TokenType::SEMICOLON) {
+                parser->consume_token();
+            }
+            return node;
+        }
+    }
+}
