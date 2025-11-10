82618757269a0576aaed9e3429bbfd91961c0a1b
weslleymirandadev <contato@weslleymiranda.dev>
2025-11-10 11:17:45 -0300

    feat(binop): added binary operation definition

---
+++ include/frontend/ast/expressions/binary_expr_node.hpp
@@ -0,0 +1,25 @@
+#pragma once
+#include "../types.hpp"
+#include <string>
+#include <memory>
+
+class BinaryExprNode : public Expr {
+public:
+    std::string op;
+    std::unique_ptr<Expr> left;
+    std::unique_ptr<Expr> right;
+
+    BinaryExprNode(std::string operator_, std::unique_ptr<Expr> lhs, std::unique_ptr<Expr> rhs)
+        : Expr(NodeType::BinaryExpression), op(std::move(operator_)), left(std::move(lhs)), right(std::move(rhs)) {}
+
+    ~BinaryExprNode() override = default;
+
+    Node* clone() const override {
+        auto cloned_left = left ? std::unique_ptr<Expr>(static_cast<Expr*>(left->clone())) : nullptr;
+        auto cloned_right = right ? std::unique_ptr<Expr>(static_cast<Expr*>(right->clone())) : nullptr;
+        return new BinaryExprNode(op, std::move(cloned_left), std::move(cloned_right));
+    }
+
+    void codegen(rph::IRGenerationContext& ctx) override;
+};
+
