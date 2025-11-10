293ce5dc2d331f8440c2085281e582095d261aec
weslleymirandadev <contato@weslleymiranda.dev>
2025-11-10 11:11:14 -0300

    feat(include): added main includes to header files

---
+++ include/frontend/ast/ast.hpp
@@ -0,0 +1,41 @@
+#pragma once
+
+// Types base
+#include "types.hpp"
+
+// Expressions
+#include "expressions/identifier_node.hpp"
+#include "expressions/numeric_literal_node.hpp"
+#include "expressions/string_literal_node.hpp"
+#include "expressions/binary_expr_node.hpp"
+#include "expressions/assignment_expr_node.hpp"
+#include "expressions/array_expr_node.hpp"
+#include "expressions/tuple_expr_node.hpp"
+#include "expressions/logical_not_expr_node.hpp"
+#include "expressions/unary_minus_expr_node.hpp"
+#include "expressions/increment_expr_node.hpp"
+#include "expressions/decrement_expr_node.hpp"
+#include "expressions/post_increment_expr_node.hpp"
+#include "expressions/post_decrement_expr_node.hpp"
+#include "expressions/access_expr_node.hpp"
+#include "expressions/member_expr_node.hpp"
+#include "expressions/call_expr_node.hpp"
+#include "expressions/key_value_node.hpp"
+#include "expressions/map_node.hpp"
+#include "expressions/list_comp_node.hpp"
+#include "expressions/conditional_expr_node.hpp"
+#include "expressions/vector_expr_node.hpp"
+#include "expressions/boolean_literal_node.hpp"
+
+// Statements
+#include "statements/return_stmt_node.hpp"
+#include "statements/declaration_stmt_node.hpp"
+#include "statements/label_stmt_node.hpp"
+#include "statements/if_statement_node.hpp"
+#include "statements/for_stmt_node.hpp"
+#include "statements/loop_stmt_node.hpp"
+#include "statements/while_stmt_node.hpp"
+#include "statements/match_stmt_node.hpp"
+
+// Program
+#include "program.hpp"
