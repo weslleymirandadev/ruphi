cb32304ad05a1edb900263282a0b3e5e292afee5
weslleymirandadev <contato@weslleymiranda.dev>
2025-11-10 11:18:31 -0300

    feat(ternary): added ternary/conditional node

---
+++ include/frontend/ast/expressions/conditional_expr_node.hpp
@@ -0,0 +1,31 @@
+#pragma once
+#include "../types.hpp"
+#include <memory>
+
+class ConditionalExprNode : public Expr {
+public:
+    std::unique_ptr<Expr> true_expr;
+    std::unique_ptr<Expr> condition;
+    std::unique_ptr<Expr> false_expr;
+
+    ConditionalExprNode(
+        std::unique_ptr<Expr> t,
+        std::unique_ptr<Expr> c,
+        std::unique_ptr<Expr> f
+    ) : Expr(NodeType::ConditionalExpression),
+        true_expr(std::move(t)),
+        condition(std::move(c)),
+        false_expr(std::move(f)) {}
+
+    ~ConditionalExprNode() override = default;
+
+    Node* clone() const override {
+        return new ConditionalExprNode(
+            std::unique_ptr<Expr>(static_cast<Expr*>(true_expr->clone())),
+            std::unique_ptr<Expr>(static_cast<Expr*>(condition->clone())),
+            std::unique_ptr<Expr>(static_cast<Expr*>(false_expr->clone()))
+        );
+    }
+
+    void codegen(rph::IRGenerationContext& ctx) override;
+};
