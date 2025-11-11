4b13ca6a8915c9212a86d68eee534b9513893605
weslleymirandadev <contato@weslleymiranda.dev>
2025-11-11 15:33:47 -0300

    feat(checker): first setup

--- include/frontend/checker/checker.hpp
+++ include/frontend/checker/checker.hpp
@@ -2,14 +2,16 @@
 #include "frontend/checker/namespace.hpp"
 #include "frontend/ast/ast.hpp"
 #include <vector>
-
+#include <unordered_map>
 namespace rph {
     class Checker {
         private:
             std::vector<std::shared_ptr<rph::Namespace>> namespaces;
             std::shared_ptr<rph::Namespace> scope;
+            std::unordered_map<std::string, std::unique_ptr<rph::Type>> types;
         public:
             Checker();
+            void getty(std::string ty);
             void push_scope();
             void pop_scope();
             std::unique_ptr<rph::Type> check_node(std::unique_ptr<Node> node);
