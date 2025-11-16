d82b87fec1ff5d8bfa04b8d17d663530d6ee8058
weslleymirandadev <contato@weslleymiranda.dev>
2025-11-16 12:37:21 -0300

    feat(bases): implemented correct bases generation/conversion

--- src/backend/codegen/expressions/generate_numeric_literal.cpp
+++ src/backend/codegen/expressions/generate_numeric_literal.cpp
@@ -1,6 +1,7 @@
 #include "frontend/ast/expressions/numeric_literal_node.hpp"
 #include "backend/codegen/ir_context.hpp"
 #include "backend/codegen/ir_utils.hpp"
+#include <iostream>
 
 void NumericLiteralNode::codegen(rph::IRGenerationContext& context) {
     // Heurística simples: contém ponto → float; senão int
@@ -10,7 +11,24 @@ void NumericLiteralNode::codegen(rph::IRGenerationContext& context) {
         return;
     }
 
-    int32_t integer = std::stoi(value);
+    int base = 10;
+    switch (value.at(1)) {
+        case 'b':
+            base = 2;
+            break;
+        case 'o':
+            base = 8;
+            break;
+        case 'x':
+            base = 16;
+            break;
+    }
+    std::cout << value << std::endl;
+    if (base != 10) {
+        value = value.substr(2, value.size());
+    }
+    std::cout << value << std::endl;
+    int32_t integer = std::stoi(value, 0, base);
     context.push_value(rph::ir_utils::create_int_constant(context, integer));
 }
 
--- src/backend/runtime/types/json.c
+++
@@ -1,6 +0,0 @@
-#include "backend/runtime/rph_runtime.h"
-
-void create_json(Value* out, Value v) {
-    *out = v;
-    out->type = TAG_JSON;
-}
