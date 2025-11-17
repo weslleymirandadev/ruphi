abda3c88750f147a68c526c4f03f9ab34bfc2ca7
weslleymirandadev <contato@weslleymiranda.dev>
2025-11-17 20:05:57 -0300

    feat(checker): added main header

---
+++ include/frontend/checker/checker_meth.hpp
@@ -0,0 +1,5 @@
+#pragma once
+#include "frontend/checker/expressions/check_primary_expr.hpp"
+#include "frontend/checker/statements/check_program.hpp"
+#include "frontend/checker/statements/check_decl_stmt.hpp"
+#include "frontend/checker/checker.hpp"
