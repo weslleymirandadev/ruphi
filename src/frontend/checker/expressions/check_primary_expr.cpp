a6e32af5ce2667c8b0e8199dfcf2faba91aaed0a
weslleymirandadev <contato@weslleymiranda.dev>
2025-11-17 20:09:37 -0300

    fix(ptr): replaced unique_ptr by shared_ptr for optimizations

--- src/frontend/checker/expressions/check_primary_expr.cpp
+++ src/frontend/checker/expressions/check_primary_expr.cpp
@@ -1,9 +1,10 @@
 #include "frontend/checker/expressions/check_primary_expr.hpp"
 #include "frontend/ast/ast.hpp"
-std::shared_ptr<rph::Type> check_primary_expr(rph::Checker* ch, std::unique_ptr<Node> node) {
+
+std::shared_ptr<rph::Type>& check_primary_expr(rph::Checker* ch, Node* node) {
     switch (node->kind) {
         case NodeType::NumericLiteral: {
-          const auto vl = std::unique_ptr<NumericLiteralNode>(static_cast<NumericLiteralNode*>(node.get()));
+          const auto* vl = static_cast<NumericLiteralNode*>(node);
           if (vl->value.find('.') != std::string::npos) {
             return ch->getty("float");
           }
@@ -14,9 +15,10 @@ std::shared_ptr<rph::Type> check_primary_expr(rph::Checker* ch, std::unique_ptr<
         case NodeType::BooleanLiteral:
           return ch->getty("bool");
         case NodeType::Identifier: {
-          const auto vl = std::unique_ptr<NumericLiteralNode>(static_cast<NumericLiteralNode*>(node.get()));
-          return ch->scope->get_key(vl->value);
+          const auto* vl = static_cast<IdentifierNode*>(node);
+          return ch->scope->get_key(vl->symbol);
         }
+        default:
+          return ch->getty("void");
     }
-    
 }
